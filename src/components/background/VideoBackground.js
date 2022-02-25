bf3b4be03f55d6e0903805ed591f09fdb201cd10
armin <armin.arndt@googlemail.com>
2022-02-25 22:22:01 +0100

    add background image

--- src/components/background/VideoBackground.js
+++ src/components/background/VideoBackground.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import ReactPlayer from "react-player";
 import PropTypes from "prop-types";
 import './videoBackground.css';
@@ -9,17 +9,39 @@ const youtubeLink = "https://www.youtube.com/watch?v=bhWJF9FlBqM";
 // vimeo > styling not yet adjusted
 const vimeoLink = "https://vimeo.com/249086569";
 
-// const videoBackground = ({ embedId }) => (
-const VideoBackground = () => (
 
-  // <ReactPlayer
-  // url={props.url}
-  // playing={true}
-  // width={props.width}
-  // height={props.height}
-  // /> 
 
-  <div id="reactVideoPlayerParent">
+// const bitmapLink = "https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1";
+const bitmapLink = "./img/defaultBG.jpg";
+const backgroundColor = "blue";
+
+const backgroundImageStyle = `
+  body{
+    background-color: ${backgroundColor};
+    z-index: -1;
+    background: url("${bitmapLink}");
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+  }
+`;
+
+function BackgroundImage() {
+  useEffect(() => {
+    const style = document.createElement('style');
+    style.innerHTML = backgroundImageStyle;
+    document.body.appendChild(style);
+    return () => { 
+      document.body.removeChild(style); 
+    }
+  }, [])
+  return <div></div>
+};
+
+function VideoBackground() {
+  return (
+    <div id="reactVideoPlayerParent">
     <div id="reactVideoPlayerCoverUp"></div>
     <ReactPlayer
     url={youtubeLink}
@@ -29,10 +51,36 @@ const VideoBackground = () => (
     id="reactVideoPlayerComponent"
     />
   </div>
-);
-
-VideoBackground.propTypes = {
-  embedId: PropTypes.string.isRequired
+  )
 };
 
-export default VideoBackground;
+// // const videoBackground = ({ embedId }) => (
+// const VideoBackground = () => (
+
+//   // <ReactPlayer
+//   // url={props.url}
+//   // playing={true}
+//   // width={props.width}
+//   // height={props.height}
+//   // /> 
+
+//   <div id="reactVideoPlayerParent">
+//     <div id="reactVideoPlayerCoverUp"></div>
+//     <ReactPlayer
+//     url={youtubeLink}
+//     playing={true}
+//     loop={true}
+//     muted={true}
+//     id="reactVideoPlayerComponent"
+//     />
+//   </div>
+// );
+
+// VideoBackground.propTypes = {
+//   embedId: PropTypes.string.isRequired
+// };
+
+export default(
+  BackgroundImage,
+  VideoBackground
+) 
