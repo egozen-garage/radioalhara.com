37ecf96e1820829fe0485ba651a9a8145039e064
Ken Rodenwaldt <kennyro@me.com>
2022-03-20 16:10:19 +0100

    add favicon + html meta tag

--- src/App.js
+++ src/App.js
@@ -2,28 +2,61 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // pages
 import AllChannels from './pages/AllChannels';
 import RadioChannel from './pages/RadioChannel';
+import { MetaTags } from "react-meta-tags";
+import ImageAssets from "./assets/img/logo192.svg";
 // import About from './pages/About';
 
 // styles
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hi :) Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <MetaTags>
+        {/* <!-- HTML Meta Tags --> */}
+        <title>Radio alHara</title>
+        <meta
+          name="description"
+          content="///// Hello World /////"
+        />
+
+        {/* <!-- Google / Search Engine Tags --> */}
+        <meta itemprop="name" content="Radio alHara" />
+        <meta
+          itemprop="description"
+          content="Radio alHara. 24/7 Online Music Broadcast to the World."
+        />
+        <meta
+          itemprop="image"
+          content= {ImageAssets}
+        />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://reedbarger.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Radio alHara" />
+        <meta
+          property="og:description"
+          content="Radio alHara. 24/7 Online Music Broadcast to the World."
+        />
+        <meta
+          property="og:image"
+          content= {ImageAssets}
+        />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Radio alHara" />
+        <meta
+          name="twitter:description"
+          content="Radio alHara. 24/7 Online Music Broadcast to the World."
+        />
+        <meta
+          name="twitter:image"
+          content={ImageAssets}
+        />
+      </MetaTags>
 
       <BrowserRouter>
             {/* Header Area */}
