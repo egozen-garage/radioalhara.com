0b4e531b8aee1819244d7928f5226cba05ca2d72
armin <armin.arndt@googlemail.com>
2022-03-20 17:14:16 +0100

    ProgramContainer.js: set up api connection + Change sanity schema text field to portable text field

--- src/components/ProgramContainer.js
+++ src/components/ProgramContainer.js
@@ -1,9 +1,37 @@
 import '../style/ProgramContainer.css'
+import {PortableText} from '@portabletext/react'
+
+
+const myPortableTextComponents = {
+    // types: {
+    //   image: ({value}) => <img src={value.imageUrl} />,
+    //   callToAction: ({value, isInline}) =>
+    //     isInline ? (
+    //       <a href={value.url}>{value.text}</a>
+    //     ) : (
+    //       <div className="callToAction">{value.text}</div>
+    //     ),
+    // },
+  
+    marks: {
+      link: ({children, value}) => {
+        const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined
+        return (
+          <a href={value.href} rel={rel}>
+            {children}
+          </a>
+        )
+      },
+    },
+  }
 
 export default function ProgramContainer(props){
     return(
         <div className='programContainer'>
-          {props.channelItem.program}
+          {/* {props.channelItem.program[0]} */}
+          <PortableText value={props.channelItem.program}
+                        components={myPortableTextComponents}
+/>
         </div>
       )
 }
