eb95fd222e5e48bd99f1aa64c4e2f227c1b5e3d2
armin <armin.arndt@googlemail.com>
2022-03-20 18:08:14 +0100

    VideoBackground.js: secure api call; call component only if api endpoint true

--- src/components/background/VideoBackground.js
+++ src/components/background/VideoBackground.js
@@ -7,25 +7,22 @@ import { getFile } from "@sanity/asset-utils";
 // import PropTypes from "prop-types";
 import './videoBackground.css';
 
-// // video testing links
-// const privateFile = "http://trust.dialectica-machina.com/video/recordat28.gif.mp4";
-// const youtubeLink = "https://www.youtube.com/watch?v=O0YYf2Xn7oA&ab_channel=thekinolibrary";
-// // vimeo > styling not yet adjusted
-// const vimeoLink = "https://vimeo.com/249086569";
+const backgroundColor = "black";
 
-// // bitmap testing links
-// // const bitmapLink = "https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1";
-// // const bitmapLink = "./img/defaultBG.jpg";
-const backgroundColor = "blue";
+function BackgroundColor() {
+  return (
+    <div className="backgroundDiv">
+      <div id="backgroundImage" style={{ backgroundColor: {backgroundColor},}}></div>
+    </div>
+  )
+};
 
 function BackgroundImage(channelItem) {
-  console.log("image url: " + channelItem.channelItem.bitmapLink);
   return (
     <div className="backgroundDiv">
       <div id="backgroundImage" style={{
       backgroundColor: {backgroundColor},
       backgroundImage: `url(${channelItem.channelItem.bitmapLink})`
-      // background: `url("https://64.media.tumblr.com/00af5b5e3e6912a5e5a56e63231bf657/tumblr_na9invhkBl1tvgexko1_400.gifv")`
       }}></div>
     </div>
   )
@@ -33,7 +30,6 @@ function BackgroundImage(channelItem) {
 
 function BackgroundVideo(channelItem) {
   const file = getFile(channelItem.channelItem.videoFile, sanityClient.config());
-  console.log(file.asset.url);
   return (
     <div className="backgroundDiv">
       <div className="reactVideoPlayerCoverUp"></div>
@@ -51,14 +47,14 @@ function BackgroundVideo(channelItem) {
 
 export default function Background(props){
   const showContent=(props)=>{
-    console.log("current medium: " + props.channelItem.backgroundMedium);
-    if(props.channelItem.backgroundMedium === "videoFile"){
+    console.log("current medium: " + props.channelItem.videoFile);
+    if(props.channelItem.backgroundMedium === "videoFile" && props.channelItem.videoFile){
       return <BackgroundVideo  channelItem={props.channelItem}/>
     } 
-    if(props.channelItem.backgroundMedium === "bitmapLink"){
+    if(props.channelItem.backgroundMedium === "bitmapLink" && props.channelItem.bitmapLink){
       return <BackgroundImage  channelItem={props.channelItem}/>
     }else{
-      return <BackgroundImage/>
+      return <BackgroundColor/>
     }
   }
   console.log("DATA Background medium: " + props.medium);
@@ -68,8 +64,3 @@ export default function Background(props){
     </div>
   )
 }
-
-// export default(
-//   BackgroundImage,
-//   BackgroundVideo
-// ) 
